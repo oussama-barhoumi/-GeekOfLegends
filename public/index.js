61ca08c789ffb40516bb449605a535e3352d1c06
oussama-barhoumi <barhoumi.oussama2005@gmail.com>
2025-12-27 21:37:35 +0100

    add heros

--- public/index.js
+++ public/index.js
@@ -42,6 +42,42 @@ class Warrior extends Character {
     console.log(this.name + " DRAB " + boss.name + " BI " + dmg);
   }
 }
+class Mage extends Character{
+    constructor(name , hp , attack){
+        super(name , hp , attack);
+        this.mana = [7 , 9 , 11 ][Math.floor(Math.random() * 3)];
+    }
+    attackBoss(boss){
+        if (this. mana < 2){
+            console.log(this.name + "t9adat lmana")
+            this.mana += 7 
+        }        
+        this.mana -= 2;
+        let dmg = this.getAttack();
+        boss.hp -= dmg;
+        console.log(this.name + "drab" + boss.name + " bi " + dmg);
+    }
+
+}
+class Archer extends Character{
+    constructor(name , hp , attack){
+        super(name , hp , attack);
+        this.arrows = [7 , 8 , 9 , 10 , 11 ][Math.floor(Math.random() * 5)];
+    }
+
+    attackBoss(boss){
+        if (this.arrows < 2){
+            console.log(this.name + "3ando sahem")
+            this.arrows += 6;
+            return
+        }
+        this.arrows -= 2
+        this.arrows += 1
+        let dmg = this.getAttack();
+        console.log(this.name + "shooots " + boss.name + "for" + dmg)
+    
+    }
+}
 
 
 
