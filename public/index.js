af32a408c6e808b53d942e75c7024bf53f34f557
oussama-barhoumi <barhoumi.oussama2005@gmail.com>
2025-12-27 19:14:58 +0100

    test the attack and the defense

--- public/index.js
+++ public/index.js
@@ -5,5 +5,30 @@ class Characters{
         this.attack = attack;
         this.stance ="normal"
     }
-    
-}
+    Setstance(stance){
+        this.stance = stance;
+    }
+    getattack(){
+        if (this.stance ==="attack")
+            return this.attack * 1.5;
+        else if ( this.stance ==="defense")
+            return this.attack * 0.5;
+        else 
+            return this.attack;
+    }
+    getdamage(damage){
+        if (this.stance ==="defense") damage = damage * 0.5;
+        this.hp -= damage;
+        console.log(`${this.name} received ${damage} damage, remaining HP: ${this.hp}`);
+    }
+}let hero = new Characters("oussama", 100, 20);
+
+console.log("Normal attack:", hero.getattack());
+
+hero.Setstance("attack");
+console.log("Attack stance:", hero.getattack());
+
+hero.Setstance("defense");
+hero.getdamage(40);
+console.log("HP after defense:", hero.hp);
+
