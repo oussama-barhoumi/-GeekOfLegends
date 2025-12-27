d39c4fc065da7d664f6d5b74146087aecac259f1
oussama-barhoumi <barhoumi.oussama2005@gmail.com>
2025-12-27 17:55:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# -GeekOfLegends
+# -GeekOfLegends
---
+++ public/index.js
@@ -0,0 +1,9 @@
+class Characters{
+    constructor(name , hp , attack){
+        this.name = name ; 
+        this.hp = hp ;
+        this.attack = attack;
+        this.stance ="normal"
+    }
+    
+}
